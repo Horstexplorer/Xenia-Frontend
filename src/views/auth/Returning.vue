c7fe6db034f38e3bb1ab4ee2b36b870177fb8ff0
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-12-13 22:26:21 +0100

    foxes yes yes

--- src/views/auth/Returning.vue
+++ src/views/auth/Returning.vue
@@ -5,20 +5,20 @@
 </template>
 
 <script>
-import HTTPCore from "../../xbd_api/core/httpcore"
+import {login} from "@/xbd_api/core/httpcore"
 
 export default {
   name: "Returning",
   mounted() {
     if(this.$route.query.code === undefined || this.$route.query.state === undefined){
       this.$router.replace({query:null});
-      this.$route.push("/");
+      this.$router.push("/");
     }else{
       let authCode = this.$route.query.code;
       let state = this.$route.query.state;
       this.$router.replace({query:null});
       // send
-      HTTPCore.login(authCode, state,
+      login(authCode, state,
           () => this.$router.push("/dashboard"),
           () => this.$router.push("/")
       );
