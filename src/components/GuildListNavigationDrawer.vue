649cd68b46e7d099ea73f6824c690590f219539a
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2021-02-03 12:56:08 +0100

    various improvements

--- src/components/GuildListNavigationDrawer.vue
+++ src/components/GuildListNavigationDrawer.vue
@@ -1,27 +1,37 @@
 <template>
   <v-navigation-drawer
-      :value="true"
+      v-model="drawer"
       clipped
   >
     <v-list shaped nav>
-      <v-list-item
-        v-for="guild in guilds"
-        :key="guild.guildId"
-        :to="`/dashboard/guilds/${guild.guildId}`"
-        exact
+      <v-list-group
+          v-for="guild in guilds"
+          :key="guild.guildId"
+          :to="`/dashboard/guilds/${guild.guildId}`"
+          :value="isSelected(guild.guildId)"
+          exact
       >
-        <v-list-item-avatar>
-          <icon
-              :icon="guild.iconUrl"
-              :alt="'${guild.guildName} guild'"
-              :text="guild.guildName"
-              :size="40"
-          />
-        </v-list-item-avatar>
-        <v-list-item-content>
-          <v-list-item-title>{{ guild.guildName }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+        <template v-slot:activator>
+          <v-list-item-avatar>
+            <icon
+                :icon="guild.iconUrl"
+                :alt="'${guild.guildName} guild'"
+                :text="guild.guildName"
+                :size="40"
+            />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ guild.guildName }}</v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item v-for="(setting, i) in settings" :key="i" :to="`/dashboard/guilds/${guild.guildId}/${setting.url}`">
+          <v-list-item-content>
+            <v-list-item-title>{{ setting.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list-group>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -35,8 +45,50 @@ export default {
       required: true,
       type: Array,
     },
+    value: {
+      required: true
+    }
   },
   components: {Icon},
+
+  data() {
+    return {
+      settings: [
+        {
+          url: "guildsettings",
+          name: "Guild Scoped Settings"
+        },
+        {
+          url: "channelsettings",
+          name: "Channel Scoped Settings"
+        },
+        {
+          url: "virtualrolesettings",
+          name: "Virtual Roles"
+        },
+      ]
+    }
+  },
+
+  computed: {
+    guildId(){
+      return this.$route.params.guildId;
+    },
+    drawer: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+
+  methods: {
+    isSelected(guildId){
+      return this.guildId == guildId
+    }
+  }
 }
 </script>
 
